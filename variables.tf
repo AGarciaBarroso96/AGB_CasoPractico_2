82f0c58ab4544338afc0f5cd0a27700c1c1054d6
AGarciaBarroso96 <122260582+AGarciaBarroso96@users.noreply.github.com>
2023-03-20 22:18:47 +0100

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,35 @@
+variable "resource_group_name" {
+  default = "rg-createdbyTF"
+}
+
+variable "location_name" {
+  default = "uksouth"
+}
+
+variable "network_name" {
+  default = "vnet1"
+}
+
+variable "subnet_name" {
+  default = "subnet1"
+}
+
+variable "pip_name" {
+  default = "vip1"
+}
+
+variable "security_group_name" {
+  default = "sg-createdbyTF"
+}
+
+variable "container_registry_name" {
+  default = "AGBcontainerRegistry1"
+}
+
+variable "kubernetes_cluster_name" {
+  default = "aks1"
+}
+
+variable "role_name" {
+  default = "acrrole1"
+}
